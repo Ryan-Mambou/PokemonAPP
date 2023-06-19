8d56e5471dce30134ed9e6f57acf0611b79f6fee
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-06-19 12:15:18 +0100

    debug on fetching with serverside components

--- Components/Pokemon.jsx
+++ Components/Pokemon.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 import pikachu from '../public/pikachu.png'
-
-export async function getStaticProps(){
-    const data = await axios.get("https://pokeapi.co/api/v2/ability/");
-    console.log('Why')
-    console.log(data)
-
-    return {
-        props: {
-            pokemons: data.data
-        }
-    }
-  }
   
 
-function Pokemon({pokemons}) {
-    console.log('pokemons', pokemons)
+function Pokemon() {
   return (
     <div>
-        <div className='flex flex-row bg-green-300 shadow-lg border-r-4 w-[20rem] h-[6rem] gap-4 items-center p-2'>
+        <div className='flex flex-row cursor-pointer bg-green-300 shadow-lg border-r-4 w-[20rem] h-[6rem] gap-4 items-center p-2 rounded'>
             <div className='rounded-full w-[4.5rem] h-[4.5rem]'>
                 <Image src={pikachu} alt='profileImg' priority="false" className='rounded-full object-cover' />
                 </div>
--- app/page.js
+++ app/page.js
@@ -2,11 +2,25 @@ import React from 'react'
 import Skeleton from '@/Components/Skeleton'
 import Pokemon from '@/Components/Pokemon'
 
-export default function Home() {
+
+export default async function Home() {
+  const pokemons = await getPokemons();
+  console.log(pokemons)
   return (
    <div className="bg-blue-500 w-full h-screen">
+    <h1 className='text-center'>Pokemons!</h1>
+    <div className='w-full md:w-10/12 m-auto flex flex-col gap-4 bg-yellow-300 items-center'>
     <Skeleton />
     <Pokemon />
+    </div>
    </div>
   )
+}
+
+export const getPokemons = async () => {
+  const res = await fetch('https://pokeapi.co/api/v2/ability', {
+    next: {revalidate: 10},
+  })
+  const data = await res.json()
+  return data
 }
