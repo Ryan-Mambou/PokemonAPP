48bf6d91bd8da2ae4d43034d12c6a16da8ba52a1
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-07-24 06:26:26 +0100

    delete unnecessary files

--- utils/Hydrate.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { Hydrate as RQHydrate, HydrateProps } from "@tanstack/react-query";
-
-function Hydrate(props) {
-  return <RQHydrate {...props} />;
-}
-
-export default Hydrate;
